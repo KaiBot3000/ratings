b3ed4c82fae23fdfe6c074b83fca983f72a3acba
Hackbright Student <info@hackbrightacademy.com>
2015-07-27 16:53:35 -0700

    working on load movies, need to convert datetime

--- seed.py
+++ seed.py
@@ -13,12 +13,23 @@ def load_users():
         split_line = line.rstrip().split("|")
         new_user = User(user_id=split_line[0], age=split_line[1], zipcode=split_line[4])
         db.session.add(new_user)
-        
+
     db.session.commit()
 
 
 def load_movies():
     """Load movies from u.item into database."""
+
+    f = open("seed_data/u.item")
+
+    for line in f:
+        split_line = line.rstrip().split("|")
+        new_movie = Movie(movie_id=split_line[0], title=split_line[1][:-7], released_at=split_line[2], imdb_url=split_line[3])
+        print new_movie.released_at
+        print new_movie
+        db.session.add(new_movie)
+
+    # db.session.commit()
 
 
 def load_ratings():
@@ -28,6 +39,6 @@ def load_ratings():
 if __name__ == "__main__":
     connect_to_db(app)
 
-    load_users()
+    # load_users()
     load_movies()
-    load_ratings()
+    # load_ratings()
