0edf514d4338b61e1cb2bd02f40f3f9411378860
Hackbright Student <info@hackbrightacademy.com>
2015-07-27 17:51:41 -0700

    finished functions to load data

--- seed.py
+++ seed.py
@@ -38,11 +38,21 @@ def load_movies():
 
 def load_ratings():
     """Load ratings from u.data into database."""
+    
+    f = open("seed_data/u.data")
+
+    for line in f:
+        split_line = line.rstrip().split()
+        new_rating = Rating(user_id=split_line[0], movie_id=split_line[1], score=split_line[2])
+        db.session.add(new_rating)
+
+    db.session.commit()
+
 
 
 if __name__ == "__main__":
     connect_to_db(app)
 
     # load_users()
-    load_movies()
-    # load_ratings()
+    #load_movies()
+    load_ratings()
