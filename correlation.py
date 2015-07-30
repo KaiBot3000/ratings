fde4125446ce56247fc706b713cbc68de43b6ca7
Hackbright Student <info@hackbrightacademy.com>
2015-07-30 12:59:50 -0700

    worked out plan for making list of tuples for pearson correlation

--- correlation.py
+++ correlation.py
@@ -33,3 +33,43 @@ def pearson(pairs):
         return 0
 
     return numerator / denominator
+
+
+def make_pairs():
+
+    """
+    What we have: list of our ratings, 
+                list of users who rated target movie, 
+                target movie object
+
+    We want: List of pairs (our rating, their rating) for each other user
+                    (this is argument for pearson() )
+
+    # we'll need to change parameter later, since we need to compare w/ multiple users
+    m = Movie.query.filter_by(title="Toy Story").one()
+    u = User.query.get(1)  
+
+    # list of user(1)'s rating objects
+    ratings = u.ratings 
+
+    # list of all ratings Toy Story has received
+    other_ratings = Rating.query.filter_by(movie_id=m.movie_id).all()
+
+    # list of all users that have rated Toy Story
+    other_users = [r.user for r in other_ratings]
+
+    # for our ratings
+        for other other_users
+            if they have same movie
+                get their score
+
+    # for our ratings
+        for other_user 
+            if our rating movie id in their list of ratings,
+                add both to list as tuple
+        
+
+    # for our ratings
+        add movie id to dictionary
+        for other user add ids to their own dictionary
+        zip dicts together, should make list of tuples
